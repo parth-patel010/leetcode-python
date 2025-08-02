887c1f7748755fa015c66e15b779850c0d1d7c2c
Parth Patel <your.email@example.com>
2025-08-02 21:15:02 +0530

    Best-time-to-buy-or-sell-stock

---
+++ buy_and_sell_stocks.py
@@ -0,0 +1,15 @@
+#Best Time to Buy and Sell Stock
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1 
+        MaxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                MaxP = max(MaxP, profit)
+            else:
+                l = r
+            r += 1  
+        return MaxP
